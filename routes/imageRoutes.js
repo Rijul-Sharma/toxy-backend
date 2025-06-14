dd8b7a6becb6510cd9d01647232924016598a6a7
Rijul-Sharma <rijul.sharma117@gmail.com>
2025-06-14 14:20:32 +0530

    added delete icon

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { uploadImage } from '../controllers/imageController.js'
+import { deleteImage, uploadImage } from '../controllers/imageController.js'
 import { upload } from '../config/cloudinary.js'
 
 const router = express.Router()
 
 router.post('/upload', upload.single('image'), uploadImage)
+router.post('/delete', deleteImage);
 
 export default router
